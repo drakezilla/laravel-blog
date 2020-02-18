6b123d4419d7cdf98b0b202b75f2b47f964556ce
drake <dt.drake@gmail.com>
2020-02-18 11:40:06 +0100

    Creado formulario de nuevos post y metodo de guardado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class HomeController extends Controller
-{
+use App\Post;
+
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -21,8 +23,21 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
+    public function index() {
         return view('home');
     }
+
+    public function getPostForm() {
+        return view('post/post_form');
+    }
+
+    public function createPost(Request $request) {
+        $post = Post::create(array(
+                    'title' => $request->input('title'),
+                    'description' => $request->input('description'),
+                    'author' => Auth::user()->id
+        ));
+        return redirect()->route('home')->with('success', 'Post has been successfully added!');
+    }
+
 }
